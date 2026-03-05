de63d58ffb20ba0c2eb1570a51efb158491addc4
katsumoto123 <sefa.aslan0614@gmail.com>
2026-03-05 17:57:39 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:41:16.204Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T14:57:38.654Z
+Commit: Resolve null pointer exception in user service
