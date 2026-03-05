63cb3f6e445c1df152c69682409871e1ce7d5f0e
katsumoto123 <sefa.aslan0614@gmail.com>
2026-03-05 21:23:08 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:22:37.622Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T18:23:08.391Z
+Commit: Fix CSS styling bug on mobile devices
