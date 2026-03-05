3e3e35f5c9f77f94fad3af5ca44e8b32d38702e8
katsumoto123 <sefa.aslan0614@gmail.com>
2026-03-05 21:21:39 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:57:36.877Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T18:21:38.341Z
+Commit: Resolve null pointer exception in user service
