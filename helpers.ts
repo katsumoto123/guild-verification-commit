2c8a094a75d646db1243bd84150395587634af63
katsumoto123 <sefa.aslan0614@gmail.com>
2026-03-05 21:22:38 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:22:36.509Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T18:22:37.622Z
+Commit: Fix broken links in documentation
