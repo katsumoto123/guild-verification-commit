1d80092b46502466f503f710783d3855031fe5cf
katsumoto123 <sefa.aslan0614@gmail.com>
2026-03-06 16:35:04 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:23:19.408Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T13:35:03.787Z
+Commit: Correct typo in configuration file
